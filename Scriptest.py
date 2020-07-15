d435bf8c8fff6301d60d15b5d2020b3321e2c267
ch0527 <670316525@qq.com>
2020-07-16 00:56:23 +0800

    commit on 'master'

--- Scriptest.py
+++ Scriptest.py
@@ -1,7 +1,20 @@
 # -*- coding:utf-8 -*-
 
 import win32gui
-import time
-from PIL import ImageGrab, Image
-import numpy as np
-import operator
+# import time
+# from PIL import ImageGrab, Image
+# import numpy as np
+# import operator
+
+
+class GameAssist:
+
+    def __init__(self, wdname):
+        """初始化"""
+
+        # 取得窗口句柄
+        self.hwnd = win32gui.FindWindow(0, wdname)
+        if not self.hwnd:
+            print("窗口找不到，请确认窗口句柄名称：【%s】" % wdname)
+            exit()
+
